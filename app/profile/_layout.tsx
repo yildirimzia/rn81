30b7a38b15849064b005c94f2b1de56a42103c82
ziya yildirim <zia.yildirimm@gmail.com>
2025-01-10 21:38:26 +0300

    Refactor ProfileLayout to enhance navigation and header options
    - Updated layout to use Stack from expo-router for improved screen management.
    - Added back navigation functionality with a custom header button using Ionicons.
    - Simplified header options for user-info and change-password screens, maintaining consistent styling.
    - Enhanced modal presentation and animation for a better user experience.

--- app/profile/_layout.tsx
+++ app/profile/_layout.tsx
@@ -1,31 +1,42 @@
-import { Stack } from 'expo-router';
-import { View } from 'react-native';
+import { router, Stack } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
 
-export default function AuthLayout() {
+export default function ProfileLayout() {
   return (
-    <View style={{ flex: 1 }}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="user-info" options={{
-             headerShown: true,
-             headerTitle: 'Bilgilerim',
-             headerTitleAlign: 'center',
-             headerTitleStyle: {
-              fontSize: 18,
-              fontWeight: 'bold',
-              color: '#000',
-             }
-            }} />
-        <Stack.Screen name="change-password" options={{
-             headerShown: true,
-             headerTitle: 'Şifremi Değiştir',
-             headerTitleAlign: 'center',
-             headerTitleStyle: {
-              fontSize: 18,
-              fontWeight: 'bold',
-              color: '#000',
-             }
-            }} />
-      </Stack>
-    </View>
-    );
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontSize: 18,
+          fontWeight: 'bold',
+          color: '#000',
+        },
+        presentation: 'modal',
+        animation: 'slide_from_bottom',
+        headerLeft: ({ tintColor }) => (
+          <TouchableOpacity 
+            onPress={() => router.back()}
+            style={{ marginLeft: 10 }}
+          >
+            <Ionicons name="arrow-back" size={24} />
+          </TouchableOpacity>
+        ),
+      }}
+    >
+      <Stack.Screen 
+        name="user-info" 
+        options={{
+          headerTitle: 'Bilgilerim',
+        }}
+      />
+      <Stack.Screen 
+        name="change-password" 
+        options={{
+          headerTitle: 'Şifremi Değiştir',
+        }}
+      />
+    </Stack>
+  );
 } 
